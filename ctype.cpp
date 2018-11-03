1ab033bea595dd286616f1f36253ebe409cde2d7
wkcn <wkcn@live.cn>
2018-11-03 22:24:05 +0800

    ignore the time of iteration

--- ctype.cpp
+++ ctype.cpp
@@ -3,11 +3,11 @@
 template <typename OP>
 void timing_op(OP op) {
     size_t loop_time = 1000000000;
-    cout << timing(loop_op(op(), loop_time)) << " ms" << endl;
+    cout << timing(loop_op(op(), loop_time)) - timing(loop_op({}, loop_time)) << " ms" << endl;
 }
 
 #define timing_binary_op(op, DType) \
-    timing_op([](){DType a=3943321.32132, b=34321439.9312311;DType c = op;})
+    timing_op([](){DType a=39.12, b=34.93;DType c = op;})
 
 #define timing_basic_binary_op(DType) \
     cout << "Test " #DType << endl; \
@@ -17,7 +17,8 @@ void timing_op(OP op) {
     timing_binary_op(a / b, DType);
 
 int main() {
-    timing_basic_binary_op(int);
+    timing_basic_binary_op(int8_t);
+    timing_basic_binary_op(int16_t);
     timing_basic_binary_op(int32_t);
     timing_basic_binary_op(int64_t);
     timing_basic_binary_op(float);
